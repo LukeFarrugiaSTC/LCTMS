c6d048b7d31bb0085852d509b47161f5770df07b
AndrewMallia1969 <andrewmallia@stcmalta.edu.mt>
2025-03-08 14:09:50 +0100

    Added API_readRoles functionality

---
+++ backend/classes/role.class.php
@@ -0,0 +1,32 @@
+<?php
+    require_once '../includes/config.php';
+    require_once 'utility.class.php';
+
+    class Role {
+        private $_roleId;
+        private $_roleName;
+
+        public function __construct() {
+            $this->conn = Dbh::getInstance()->getConnection();
+        }
+
+        // Getters and Setters
+        public function setRoleId($var) { $this->_roleId = $var; }
+        public function setRoleName($var) { $this->_roleName = $var; }
+
+        public function getRoleId() { return $this->_roleId; }
+        public function getRoleName() { return $this->_roleName; }
+
+        public function roleRead() {
+            try {
+                $query = "SELECT * FROM roles";
+                $stmt = $this->conn->prepare($query);
+                $stmt->execute();
+                $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return json_encode($result);
+            } catch (PDOException $e) {
+                return json_encode(["status" => "error", "message" => $e->getMessage()]);
+            }
+        }
+    }
+?>
