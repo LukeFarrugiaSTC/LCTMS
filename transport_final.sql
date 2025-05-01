0b8c514aa8cdf9485f1eb226df71d50a57b91b37
mattrax-44 <matthewcurmi4@gmail.com>
2025-05-01 11:11:06 +0200

    Add files via upload

---
+++ transport_final.sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 01, 2025 at 11:06 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `transport`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `booking_id` int(255) NOT NULL,
+  `client_id` int(255) NOT NULL COMMENT 'user_id as a foreign key from users table',
+  `destination_id` int(255) NOT NULL COMMENT 'foreign key from destination table',
+  `booking_status` varchar(255) NOT NULL,
+  `booking_date` date NOT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `modified_by` varchar(100) DEFAULT NULL,
+  `modified_date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `destinations`
+--
+
+CREATE TABLE `destinations` (
+  `destination_id` int(11) NOT NULL,
+  `destination_name` varchar(255) NOT NULL,
+  `street_id` int(11) NOT NULL COMMENT 'foreign key',
+  `town_id` int(11) NOT NULL COMMENT 'foreign key'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `role_id` int(11) NOT NULL,
+  `role_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `streets`
+--
+
+CREATE TABLE `streets` (
+  `street_id` int(11) NOT NULL,
+  `street_name` varchar(255) NOT NULL,
+  `street_longitude` int(11) NOT NULL,
+  `street_latitude` int(11) NOT NULL,
+  `town_id` int(11) NOT NULL COMMENT 'foreign key'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `towns`
+--
+
+CREATE TABLE `towns` (
+  `town_id` int(11) NOT NULL,
+  `town_name` varchar(255) NOT NULL,
+  `district_code` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_surname` varchar(255) NOT NULL,
+  `user_address` varchar(400) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_DOB` date NOT NULL,
+  `user_mobile` int(8) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_pin` int(11) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_date` date NOT NULL,
+  `role_id` int(11) NOT NULL COMMENT 'foreign key',
+  `street_id` int(11) NOT NULL COMMENT 'foreign key',
+  `town_id` int(11) NOT NULL COMMENT 'foreign key'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `user_id-foreign` (`client_id`),
+  ADD KEY `destination_id-foreign` (`destination_id`);
+
+--
+-- Indexes for table `destinations`
+--
+ALTER TABLE `destinations`
+  ADD PRIMARY KEY (`destination_id`),
+  ADD KEY `fk-street_id` (`street_id`),
+  ADD KEY `town_id-fk` (`town_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `streets`
+--
+ALTER TABLE `streets`
+  ADD PRIMARY KEY (`street_id`),
+  ADD KEY `town_id-foreign` (`town_id`);
+
+--
+-- Indexes for table `towns`
+--
+ALTER TABLE `towns`
+  ADD PRIMARY KEY (`town_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `street_id-foreign` (`street_id`),
+  ADD KEY `role_id-foreign` (`role_id`),
+  ADD KEY `fk-town_id` (`town_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `booking_id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `destinations`
+--
+ALTER TABLE `destinations`
+  MODIFY `destination_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `streets`
+--
+ALTER TABLE `streets`
+  MODIFY `street_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `towns`
+--
+ALTER TABLE `towns`
+  MODIFY `town_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `destination_id-foreign` FOREIGN KEY (`destination_id`) REFERENCES `destinations` (`destination_id`),
+  ADD CONSTRAINT `user_id-foreign` FOREIGN KEY (`client_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `destinations`
+--
+ALTER TABLE `destinations`
+  ADD CONSTRAINT `fk-street_id` FOREIGN KEY (`street_id`) REFERENCES `streets` (`street_id`),
+  ADD CONSTRAINT `town_id-fk` FOREIGN KEY (`town_id`) REFERENCES `towns` (`town_id`);
+
+--
+-- Constraints for table `streets`
+--
+ALTER TABLE `streets`
+  ADD CONSTRAINT `town_id-foreign` FOREIGN KEY (`town_id`) REFERENCES `towns` (`town_id`);
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `fk-town_id` FOREIGN KEY (`town_id`) REFERENCES `towns` (`town_id`),
+  ADD CONSTRAINT `role_id-foreign` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
+  ADD CONSTRAINT `street_id-foreign` FOREIGN KEY (`street_id`) REFERENCES `streets` (`street_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
